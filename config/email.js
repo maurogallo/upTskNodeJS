ad95d320478175138b1a327c75fc9c0a2c1f0710
maurogallo <maurogallo@gmail.com>
2019-06-05 00:21:13 -0500

    Cofigurando Nodemailer y enviando un mail de prueba

---
+++ config/email.js
@@ -0,0 +1,6 @@
+module.exports = {
+    user: 'aa9bd3f444a9e3',
+    pass: '31680962706cc7',
+    host: 'smtp.mailtrap.io',
+    port: '2525'
+}
--- index.js
+++ index.js
@@ -84,3 +84,5 @@ app.use((req, res, next) => {
 app.use('/', routes());
 
 app.listen(3000);
+
+require('./handlers/email');
