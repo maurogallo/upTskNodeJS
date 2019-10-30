8818f7fa631cb1700681d2e1937392cf6ca88f99
Mauricio <maurogallo@gmail.com>
2019-10-30 11:11:49 -0500

    Bump sequelize from 5.7.1 to 5.15.1

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 // Option 1: Passing parameters separately
 const db = new Sequelize('uptasknode', 'root', 'root', {
-  host: '127.0.0.1',
+  host: 'localhost',
   dialect:  'mysql' ,
   port: '3306',
   freezeTableName: true,
